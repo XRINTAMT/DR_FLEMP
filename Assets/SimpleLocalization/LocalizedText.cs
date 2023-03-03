b4bf8d4f88939ace2a580b13fbd56851192f2d76
Oleg-A-LLIto <Fedol1408@gmail.com>
2023-03-03 19:27:59 +0300

    Added localization asset (not setup yet)

---
+++ Assets/SimpleLocalization/LocalizedText.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Assets.SimpleLocalization
+{
+	/// <summary>
+	/// Localize text component.
+	/// </summary>
+    [RequireComponent(typeof(Text))]
+    public class LocalizedText : MonoBehaviour
+    {
+        public string LocalizationKey;
+        bool localized;
+
+        void Start()
+        {
+            /*
+            TryLocalize();
+            yield return new WaitForSecondsRealtime(0.2f);
+            TryLocalize();
+            LocalizationManager.LocalizationChanged += Localize;
+            */
+            Localize();
+            LocalizationManager.LocalizationChanged += Localize;
+        }
+
+        private void TryLocalize()
+        {
+            /*
+            try
+            {
+                Localize();
+                
+                localized = true;
+            }
+            catch
+            {
+                Debug.LogError("Failed to localize, trying again!");
+                GetComponent<Text>().text = "Failed to localize, trying again!";
+            }
+            */
+
+        }
+
+        public void OnDestroy()
+        {
+            LocalizationManager.LocalizationChanged -= Localize;
+        }
+
+        public void Localize()
+        {
+            GetComponent<Text>().text = LocalizationManager.Localize(LocalizationKey);
+        }
+
+        void Update()
+        {
+            /*
+            if (!localized)
+                TryLocalize();
+            */
+        }
+    }
+}

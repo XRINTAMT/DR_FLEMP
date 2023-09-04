0129f0113ccd21d1dfc05cd45f0b07004d8d30ef
Roman <murman89@yandex.ru>
2023-09-04 15:27:15 +0300

    add hints

--- Assets/Scripts/HintBehaviour.cs
+++ Assets/Scripts/HintBehaviour.cs
@@ -1,32 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using Autohand;
+using Autohand.Demo;
 using UnityEngine;
 
 public class HintBehaviour : MonoBehaviour
 {
     public static bool startFirstTime;
+    public static bool playAudioFirstTime;
+    public AudioClip[] audioClip;
+    public GameObject hintSkip;
+    public GameObject lineSkip;
+    AudioSource audioSource;
     private void Awake()
     {
         if (!startFirstTime)
         {
-            HintControllers [] hintControllers = FindObjectsOfType<HintControllers>();
+            HintControllers[] hintControllers = FindObjectsOfType<HintControllers>();
             for (int i = 0; i < hintControllers.Length; i++)
             {
                 hintControllers[i].EnableHint();
             }
+
+            FindObjectOfType<XRMovementControls>().SwitchLocomotion(2);
+            GetComponent<HandTriggerAreaEvents>().enabled = true;
             startFirstTime = true;
         }
+        audioSource = GetComponent<AudioSource>();
+        PlayAudio(0);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void PlayAudio(int index)
     {
-        if (startFirstTime)
+        if (!playAudioFirstTime)
         {
-            GetComponent<PlacePoint>().enabled = false;
+            audioSource.Stop();
+            audioSource.PlayOneShot(audioClip[index]);
+            if (index == 4)
+            {
+                Invoke("ChangeAudio", 24f);
+            }
         }
+
+
     }
+    void ChangeAudio() 
+    {
+        audioSource.PlayOneShot(audioClip[5]);
+        lineSkip.SetActive(true);
+        hintSkip.SetActive(true);
+
+    }
+
+    public void EnnableMove() 
+    {
+        FindObjectOfType<XRHandPlayerControllerLink>().moveAxis = Common2DAxis.primaryAxis;
+    }
+    // Start is called before the first frame update
 
 
 }

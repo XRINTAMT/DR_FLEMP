e46f8a50bd061099841f9f450839af2b9309de75
Nicklas Jonsson <nicklas.tim.jonsson@gmail.com>
2023-09-14 19:55:41 +0200

    added hardcoded functionDescriptions (for dev)

--- Assets/AICharacter/Character.cs
+++ Assets/AICharacter/Character.cs
@@ -14,11 +14,43 @@ namespace AICharacter
         public string Description;
         public string Gender;
 
-        public List<FunctionDescription> functionDescriptions;
 
         private ChatHistory ChatHistory {get; set;}
 
         private int lenOfThinkingPhrases = 3;
+
+        // !!! Change this into something more Dynamic.. !!!
+        public string contentPain =  "{\"pain\":\"9\"}";
+        public List<FunctionDescription> functionDescriptions = new List<FunctionDescription>
+            {
+                new FunctionDescription
+                {
+                    Name = "report_pain_level",
+                    Description = "Patient's pain as a number between 1 and 10.",
+                    Parameters = new Parameters
+                    {
+                        Type = "object",
+                        Properties = new Dictionary<string, Property>
+                        {
+                            {
+                                "Pain", new Property
+                                {
+                                    Type = "number",
+                                    Description = "A number from one to ten"
+                                }
+                            },
+                            {
+                                "Reponse", new Property
+                                {
+                                    Type = "string",
+                                    Description = "The in-character response. How the patient responds to the question."
+                                }
+                            }
+                        },
+                        Required = new List<string> { "pain", "reponse" }
+                    }
+                }
+            };
 
         public Character() {}
 

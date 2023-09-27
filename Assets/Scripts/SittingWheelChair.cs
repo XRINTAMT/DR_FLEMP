603f151f4aeffce928fb6490b4460ab169b18bf5
Roman <murman89@yandex.ru>
2023-09-27 13:39:33 +0300

    add patients prefabs

--- Assets/Scripts/SittingWheelChair.cs
+++ Assets/Scripts/SittingWheelChair.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SittingWheelChair : MonoBehaviour
 {
     public Transform siitingPos;
-    public HeadCharacterFollow headCharacterFollow;
+    HeadCharacterFollow headCharacterFollow;
 
     // Start is called before the first frame update
     void Start()

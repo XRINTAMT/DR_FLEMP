86996edb60d31c6d55ae8e02867bfc0a687c2d1b
Markus Åkerlund Larsson <markus.akerlund.larsson@gmail.com>
2023-11-08 09:50:39 +0100

    Lnaguage options for tablet checklists

--- Assets/Scripts/ChecklistMechanic.cs
+++ Assets/Scripts/ChecklistMechanic.cs
@@ -28,7 +28,8 @@ public class ChecklistMechanic : MonoBehaviour
 
     void ParseTheScenario() //use with caution, erases all the answers gathered from the player
     {
-        CSVParser Scenario = new CSVParser("Scenarios/" + scenarioName + "/NursingTablets");
+        var langTag = PlayerPrefs.GetInt("StudyLanguage", 0) == 0 ? "" : "German";
+        CSVParser Scenario = new CSVParser("Scenarios/" + scenarioName + "/NursingTablets" +langTag);
         int i = -1;
         correctAnswers = new int[Scenario.rowData.Count];
         givenAnswers = new int[Scenario.rowData.Count];

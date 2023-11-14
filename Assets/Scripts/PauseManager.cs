e2a73941027f29cbbddb2f6726d6e602fde45a6b
Oleg-A-LLIto <Fedol1408@gmail.com>
2023-11-14 23:13:57 +0400

    Feature: reposition the stt display and render it above everything

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -55,7 +55,9 @@ public class PauseManager : MonoBehaviour
         }
         */
         //UICamera.gameObject.GetComponent<Camera>().enabled = true;
-        UICamera.cullingMask = (1 << LayerMask.NameToLayer("Hand")) | (1 << LayerMask.NameToLayer("MenusUI")) | (1 << LayerMask.NameToLayer("Fade"));
+
+        //UICamera.cullingMask = (1 << LayerMask.NameToLayer("Hand")) | (1 << LayerMask.NameToLayer("MenusUI")) | (1 << LayerMask.NameToLayer("Fade"));
+        
         //MainCamera.cullingMask = (~0) &~ (1 << LayerMask.NameToLayer("Dialogue"));
         PauseMenu.SetActive(true);
         PauseMenu.transform.position = MenuOffset.transform.position;
@@ -97,7 +99,9 @@ public class PauseManager : MonoBehaviour
         if (Controls != null) Controls.SwitchLocomotion(PlayerPrefs.GetInt("MovementType", 0));
 
         //MainCamera.cullingMask = ~0;
-        UICamera.cullingMask = (1 << LayerMask.NameToLayer("Fade"));
+
+        //UICamera.cullingMask = (1 << LayerMask.NameToLayer("Fade"));
+
         //UICamera.gameObject.GetComponent<Camera>().enabled = false;
         GetComponent<UnscaleMove>().Play();
     }

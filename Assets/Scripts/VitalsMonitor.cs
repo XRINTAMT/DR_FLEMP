c0be5bfcdb5a07044d8ee858fde60fb036d8b375
Roman <murman89@yandex.ru>
2023-11-22 04:53:01 +0300

    add logic

--- Assets/Scripts/VitalsMonitor.cs
+++ Assets/Scripts/VitalsMonitor.cs
@@ -52,8 +52,8 @@ public class VitalsMonitor : MonoBehaviour
             //Debug.Log("{ \n \"name\": \"" + VitalValues[i].Name + "\",\n \"value\": " + (int)VitalValues[i].Value + "\n },");
         }
 
-        for (int i = 0; i < VitalValues.Length; i++)
-            Connect(i);
+        //for (int i = 0; i < VitalValues.Length; i++)
+        //    Connect(i);
     }
 
     //changes a given vital value linearly

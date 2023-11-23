f58a889b74db6bd790ab06b17460fa85a5f0d145
Roman <murman89@yandex.ru>
2023-11-23 05:25:21 +0300

    change Scripts

--- Assets/Scripts/SensorController.cs
+++ Assets/Scripts/SensorController.cs
@@ -8,7 +8,13 @@ public class SensorController : MonoBehaviour
     [SerializeField] GameObject sensor1;
     [SerializeField] GameObject sensor2;
     [SerializeField] GameObject sensor3;
+    [SerializeField] GameObject thermometer1;
+    [SerializeField] GameObject thermometer2;
+    [SerializeField] GameObject thermometer3;
     public UnityEvent putSensor;
+    public UnityEvent putThermometer;
+    public bool sensorAnimation;
+    public bool thermometerAnimation;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +22,38 @@ public class SensorController : MonoBehaviour
     }
     public void TakeSensor()
     {
-        sensor1.SetActive(false);
-        sensor2.SetActive(true);
+        if (sensorAnimation)
+        {
+            sensor1.SetActive(false);
+            sensor2.SetActive(true);
+        }
+        if (thermometerAnimation)
+        {
+            thermometer1.SetActive(false);
+            thermometer2.SetActive(true);
+        }
+
     }
 
     public void PutSensor()
     {
-        sensor2.SetActive(false);
-        sensor3.SetActive(true);
-        putSensor?.Invoke();
+        if (sensorAnimation)
+        {
+            sensor2.SetActive(false);
+            sensor3.SetActive(true);
+            putSensor?.Invoke();
+            sensorAnimation = false;
+        }
+        if (thermometerAnimation)
+        {
+            thermometer2.SetActive(false);
+            thermometer3.SetActive(true);
+            putThermometer?.Invoke();
+            thermometerAnimation = false;
+        }
+
     }
+
 }
 // Update is called once per frame
 

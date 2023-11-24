ce804af7275879b5f2e1b1bcfa46d09cfe292b53
Markus Åkerlund Larsson <markus.akerlund.larsson@gmail.com>
2023-11-24 11:13:24 +0100

    correcting some strange characters

--- Assets/Scripts/VitalsMonitor.cs
+++ Assets/Scripts/VitalsMonitor.cs
@@ -32,7 +32,7 @@ public class VitalsMonitor : MonoBehaviour
     //[SerializeField] TaskSpecificValues DataInterface;
     [SerializeField] UnityEvent OnAllConnected;
     private Coroutine AlarmCoroutine;
-    public Action<int> conne�t;
+    public Action<int> connect;
     public Action<bool> alarm;
     public Action<int,float> changeValue;
     VitalValue[] VitalValuesSaved;
@@ -175,7 +175,7 @@ public class VitalsMonitor : MonoBehaviour
     {
         if (!PhotonManager._viewerApp) 
         {
-            conne�t?.Invoke(n);
+            connect?.Invoke(n);
         } 
 
         if (VitalValues[n].Connected)

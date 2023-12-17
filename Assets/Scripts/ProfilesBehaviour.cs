96894923826eb918b84d99251035cdb67766fd6a
Roman <murman89@yandex.ru>
2023-12-17 22:22:24 +0300

    add keyboard

--- Assets/Scripts/ProfilesBehaviour.cs
+++ Assets/Scripts/ProfilesBehaviour.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 
 public class ProfilesBehaviour : MonoBehaviour
 {
-    [SerializeField] TMP_Dropdown dropdown;
-    //[SerializeField] TMP_InputField inputField;
-    List <string> options = new List<string>();
+    [SerializeField] TMP_Dropdown dropdownPatients;
+    [SerializeField] TMP_Dropdown dropdownLanguage;
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] string [] names;
+    [SerializeField] List <string> options = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
-
-        int count = PlayerPrefs.GetInt("CountPlayerID",0);
-        for (int i = 0; i < count; i++)
-            options.Add("Player " + (options.Count+1));
-        dropdown.AddOptions(options);
+        dropdownLanguage.onValueChanged.AddListener(ChooseLanguage);
+        //int count = PlayerPrefs.GetInt("CountPlayerID",0);
+        //for (int i = 0; i < count; i++)
+        //    options.Add("Player " + (options.Count+1));
+        names = PlayerPrefs.GetString("NamesPlayer").Split();
+       
+        for (int i = 0; i < names.Length; i++)
+        {
+            options.Add(names[i]);
+        }
+        dropdownPatients.AddOptions(options);
         PlayerPrefs.GetInt("CurrentPlayerID", 0);
 
-        dropdown.onValueChanged.AddListener(ChoosePlayer);
-
+        dropdownPatients.onValueChanged.AddListener(ChoosePlayer);
     }
     public void CreatePlayer() 
     {
-        options.Add("Player " + (options.Count+1));
-        dropdown.ClearOptions();
-        dropdown.AddOptions(options);
+        //options.Add("Player " + (options.Count+1));
+        if (PlayerPrefs.GetString("NamesPlayer") == "")
+        {
+            PlayerPrefs.SetString("NamesPlayer", inputField.text);
+        }
+        if (PlayerPrefs.GetString("NamesPlayer") != "")
+        {
+            PlayerPrefs.SetString("NamesPlayer", PlayerPrefs.GetString("NamesPlayer") + "\n" + inputField.text);
+        }
+        options.Add(inputField.text);
+        dropdownPatients.ClearOptions();
+        dropdownPatients.AddOptions(options);
 
         PlayerPrefs.SetInt("CurrentPlayerID", (options.Count-1));
         PlayerPrefs.SetInt("CountPlayerID", options.Count);
@@ -36,6 +52,29 @@ public class ProfilesBehaviour : MonoBehaviour
     {
         PlayerPrefs.SetInt("CurrentPlayerID", index);
         Debug.Log(PlayerPrefs.GetInt("CurrentPlayerID", 0));
+    }
+    public void ChooseLanguage(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                PlayerPrefs.SetString("Language", "English");
+                break;
+            case 1:
+                PlayerPrefs.SetString("Language", "German");
+                break;
+            case 2:
+                PlayerPrefs.SetString("Language", "Lithuanian");
+                break;
+            case 3:
+                PlayerPrefs.SetString("Language", "Latvian");
+                break;
+            case 4:
+                PlayerPrefs.SetString("Language", "Swedish");
+                break;
+            default:
+                break;
+        }
     }
 
     // Update is called once per frame

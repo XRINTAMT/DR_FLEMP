da1bbcdab3d9db0a10b4132893b3a92a752fa2b1
Oleg-A-LLIto <Fedol1408@gmail.com>
2023-12-21 21:34:44 +0400

    Feature: a button to go to the next scenario in this "campaign"

--- Assets/Scripts/SpawnLobby.cs
+++ Assets/Scripts/SpawnLobby.cs
@@ -9,6 +9,7 @@ public class SpawnLobby : MonoBehaviour
     public static int indexScene;
     public static string nameScene;
     Transform player;
+    Transform currentTransformToGoTo;
     Dictionary<string, int> Rooms = new Dictionary<string, int>
         {
             { "MRDT Room", 1 },
@@ -21,26 +22,91 @@ public class SpawnLobby : MonoBehaviour
             { "B21", 3 },
             { "B22", 3 }
         };
-    
-    // Start is called before the first frame update
+
+    Dictionary<string, int>[] Campaigns = new Dictionary<string, int>[4];
+    int[] CampaignDefaults = new int[4] { 0, 0, 4, 4 };
+
     void Awake()
     {
+        Campaigns[0] = new Dictionary<string, int>
+        {
+            { "MRDT1", 1 }
+        };
+        Campaigns[1] = new Dictionary<string, int>
+        {
+            { "A1", 2 },
+            { "A2", 1 },
+            { "A3", 1 }
+        };
+        Campaigns[2] = new Dictionary<string, int>
+        {
+            { "B11", 3 },
+            { "B12", 3 }
+        };
+        Campaigns[3] = new Dictionary<string, int>
+        {
+            { "B21", 3 },
+            { "B22", 3 }
+        };
+
         if (!nameScene.Equals("Lobby"))
         {
-            Debug.Log(nameScene);
-            player = FindObjectOfType<AutoHandPlayer>().transform.root;
-            player.position = pos[Rooms[nameScene]].position;
-            Invoke("TurnPlayer", 0.01f);
+            currentTransformToGoTo = pos[Rooms[nameScene]];
+            GoToTransformRoot();
         }
+
+        
         
         //if (indexScene != 0)
         //    player.position = pos[indexScene - 1].position;
     }
 
+    public void GoToNext()
+    {
+        int CurrentCampaign = PlayerPrefs.GetInt(PlayerPrefs.GetInt("CurrentPlayerID", 0).ToString() + "SelectedStage", 1);
+        Debug.Log("Current campaign: " + CurrentCampaign);
+        CurrentCampaign -= 1;
+        Debug.Log("Current campaign: " + CurrentCampaign);
+        int _posToGoTo = CampaignDefaults[CurrentCampaign];
+        Debug.Log("Shifting to the next one in " + Campaigns[CurrentCampaign]);
+        foreach (KeyValuePair<string, int> kvp in Campaigns[CurrentCampaign])
+        {
+            Debug.Log("Checking " + PlayerPrefs.GetInt("CurrentPlayerID", 0).ToString() + kvp.Key);
+            Debug.Log(PlayerPrefs.GetInt(PlayerPrefs.GetInt("CurrentPlayerID", 0).ToString() + kvp.Key, 0));
+            if (PlayerPrefs.GetInt(PlayerPrefs.GetInt("CurrentPlayerID", 0).ToString() + kvp.Key, 0) != 0)
+            {
+                Debug.Log("Completed " + kvp.Key);
+                _posToGoTo = kvp.Value;
+            }
+            else
+            {
+                break;
+            }
+        }
+        currentTransformToGoTo = pos[_posToGoTo];
+        GoToTransform();
+    }
+
+    private void GoToTransformRoot()
+    {
+        player = FindObjectOfType<AutoHandPlayer>().transform.root;
+        player.position = currentTransformToGoTo.position;
+        Invoke("TurnPlayer", 0.01f);
+    }
+
+    private void GoToTransform()
+    {
+        player = FindObjectOfType<AutoHandPlayer>().transform;
+        player.position = currentTransformToGoTo.position;
+        Invoke("TurnPlayer", 0.01f);
+    }
+
     private void TurnPlayer()
     {
         Transform head = FindObjectOfType<AutoHandPlayer>().headCamera.transform;
-        Debug.Log("Turning the player to " + new Vector3(player.localRotation.eulerAngles.x, pos[Rooms[nameScene]].rotation.eulerAngles.y - head.localRotation.eulerAngles.y, player.localRotation.eulerAngles.z));
-        player.localRotation = Quaternion.Euler(new Vector3(player.localRotation.eulerAngles.x, pos[Rooms[nameScene]].rotation.eulerAngles.y - head.localRotation.eulerAngles.y, player.localRotation.eulerAngles.z));
+        Debug.Log("Turning the player to " + new Vector3(player.localRotation.eulerAngles.x, currentTransformToGoTo.rotation.eulerAngles.y - head.localRotation.eulerAngles.y, player.localRotation.eulerAngles.z));
+        player.localRotation = Quaternion.Euler(new Vector3(player.localRotation.eulerAngles.x, currentTransformToGoTo.rotation.eulerAngles.y - head.localRotation.eulerAngles.y, player.localRotation.eulerAngles.z));
     }
+
+    
 }

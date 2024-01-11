c5fbbbf190a0a68f8c2bb09031a5b957e5e27833
Oleg-A-LLIto <Fedol1408@gmail.com>
2024-01-11 23:45:11 +0400

    Feature: load welcome text from file ("WelcomeTexts" in DRFlemp scenario files)

--- Assets/Scripts/WelcomeScreenController.cs
+++ Assets/Scripts/WelcomeScreenController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class WelcomeScreenController : MonoBehaviour
 {
     [SerializeField] ScenarioRelations Scenarios;
@@ -36,8 +37,7 @@ public class WelcomeScreenController : MonoBehaviour
             FindObjectOfType<RecordedScenarioText>().PlayOnAwake = false;
 
         panelStart.SetActive(true);
-        //textStart.text = Parse(SceneManager.GetActiveScene().buildIndex,1);
-        //textEnd.text = Parse(SceneManager.GetActiveScene().buildIndex,2);
+        textStart.text = GetWelcomeText();
     }
 
     string Parse(int rowIndex, int column)
@@ -46,6 +46,27 @@ public class WelcomeScreenController : MonoBehaviour
         string text = cSVParser.rowData[rowIndex][column];
         return text;
     }
+
+    private string GetWelcomeText()
+    {
+        List<string[]> csvData = (new CSVParser("Scenarios/Intros")).rowData;
+
+        int language = PlayerPrefs.GetInt(PlayerPrefs.GetInt("CurrentPlayerID", 0).ToString() + "StudyLanguage", 0);
+
+        // Find the row that matches the specified sceneName
+        string[] matchingRow = csvData.Find(row => row[0].Equals(sceneName));
+
+        if (matchingRow != null)
+        {
+            return matchingRow[language + 1];
+        }
+        else
+        {
+            Debug.LogWarning($"Scene Name '{sceneName}' not found in the CSV data.");
+            return $"Welcome text for '{sceneName}' (not implemented yet)";
+        }
+    }
+
     public void OpenEndPanel()
     {
         panelEnd.SetActive(true);

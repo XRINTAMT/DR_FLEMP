5eb5cf1b41052a3bc4e1cdfbae171cb29b00bd6e
Roman <murman89@yandex.ru>
2024-01-29 15:29:51 +0300

    add mechanic

--- Assets/Scripts/NewSyringe.cs
+++ Assets/Scripts/NewSyringe.cs
@@ -1,14 +1,13 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Events;
 using System.Linq;
-using ScenarioSystem;
 using Autohand;
+using ScenarioSystem;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
-public class Syringe : DataSaver
+public class NewSyringe : MonoBehaviour
 {
     [SerializeField] bool setupInInspector = false;
     [SerializeField] InjectionManager Manager;
@@ -25,12 +24,11 @@ public class Syringe : DataSaver
     [SerializeField] Text AmountText;
     [SerializeField] Text SubstanceText;
     [SerializeField] Vector3 Offset;
-    [SerializeField] Rigidbody SyringeCap;
     bool Guided;
     [SerializeField] Material LiquidRight;
     [SerializeField] Material LiquidTooMuch;
-    [SerializeField] GameObject ExpiredHint;
 
+    TaskSpecificValues DataInterface;
     Ampule med;
     bool inserted;
     bool pulling;
@@ -39,14 +37,27 @@ public class Syringe : DataSaver
     Vector3 innerPartPositionInit;
     Vector3 LiquidPositionInit;
     MeshRenderer liquidRenderer;
-    Material LiquidNormal;
-    Material SavedLiquidMat;
-
-    public Dictionary<string, float> ingredients { private set; get;}
-    private Dictionary<string, float> savedIngredients;
+    public Material LiquidNormal;
+    public Material LiquidNormalUncorrect;
+    public Material LiquidNormalCorrect;
+    public Dictionary<string, float> ingredients { private set; get; }
     public Injection Lable { get; private set; }
 
-    
+    [SerializeField] Transform indicate;
+    bool checkPositionInner;
+    public float syringeML;
+    public float amount;
+    public string currentSubstance;
+    public float amountDopamine;
+    public float amountSaline;
+    public float currentAmountSyringe;
+    public float currentAmountSubstance;
+    public bool dopamine;
+    public bool saline;
+
+    float inaccuracy = 0.5f;
+    Material indicateMaterial;
+
     void Awake()
     {
         innerPartPositionInit = InnerPart.transform.localPosition;
@@ -55,22 +66,72 @@ public class Syringe : DataSaver
 
         ingredients = new Dictionary<string, float>();
         med = null;
+        Head = FindObjectOfType<AutoHandPlayer>().headCamera;
         if (!setupInInspector)
         {
+            DataInterface = GetComponent<TaskSpecificValues>();
             //Debug.Log("Getting syringe capacity: "+DataInterface.TryGetItem("SyringeCapacity", ref SyringeCapacity));
-            PlayerObject player = FindObjectOfType<PlayerObject>();
-            Head = player.Head;
-            //if (!Pomp) Pomp = GameObject.FindGameObjectWithTag("PumpSyringePlacePoint");
-            Manager = FindObjectOfType<InjectionManager>();
+            //PlayerObject player = FindObjectOfType<PlayerObject>();
+            Head = FindObjectOfType<AutoHandPlayer>().headCamera;
         }
         if (Guided)
         {
             liquidRenderer = Liquid.GetComponent<MeshRenderer>();
             LiquidNormal = liquidRenderer.material;
-            SavedLiquidMat = LiquidNormal;
         }
     }
+    private void Start()
+    {
+        InnerPart.GetComponent<Grabbable>().onGrab.AddListener(CheckPositionInner);
+        InnerPart.GetComponent<Grabbable>().onRelease.AddListener(StopCheckPositionInner);
+        indicateMaterial = indicate.GetComponentInChildren<Renderer>().material;
 
+    }
+    void CheckPositionInner(Hand hand, Grabbable grabbable)
+    {
+        //indicate.gameObject.SetActive(true);
+        med = GetComponent<NewSyringeMechanic>().bottle.GetComponent<Ampule>();
+        currentSubstance = med.Substance;
+        if (currentSubstance == "Dopamine")
+        {
+            dopamine = true;
+            amount = 20;
+        }
+        if (currentSubstance == "Saline")
+        {
+            saline = true;
+            amount = 50;
+        }
+        currentAmountSyringe = syringeML;
+        checkPositionInner = true;
+    }
+    void StopCheckPositionInner(Hand hand, Grabbable grabbable)
+    {
+        //indicate.gameObject.SetActive(false);
+        if (currentSubstance == "Dopamine")
+        {
+            amountDopamine = currentAmountSubstance;
+            //med.Amount -= currentAmount;
+            //currentSubstance = "";
+        }
+        if (currentSubstance == "Saline")
+        {
+            amountSaline = currentAmountSubstance;
+            //med.Amount -= currentAmount;
+            //currentSubstance = "";
+        }
+        //AmountText.text = syringeML.ToString("0.0");
+        if (Mathf.Round(amountDopamine) == 20 && Mathf.Round(amountSaline) == 50)
+        {
+            Debug.Log("amountCorrect");
+            OnRequirementsMet?.Invoke();
+        }
+
+        indicate.GetComponentInChildren<Renderer>().material = LiquidNormal;
+        dopamine = false;
+        saline = false;
+        checkPositionInner = false;
+    }
     public void TurnTheHUD(bool OnOff)
     {
         MeasurementCanvas.SetActive(OnOff);
@@ -97,52 +158,38 @@ public class Syringe : DataSaver
         {
             if (substance == "Dopamine")
             {
-                if (ingredients.ContainsKey(substance))
+                if (Mathf.Round(ingredients[substance]) == 20)
                 {
-                    if (Mathf.Round(ingredients[substance]) == 20)
-                    {
-                        liquidRenderer.material = LiquidRight;
-                    }
-                    else
-                    {
-                        if (ingredients[substance] > 20)
-                        {
-                            liquidRenderer.material = LiquidTooMuch;
-                        }
-                        else
-                        {
-                            liquidRenderer.material = LiquidNormal;
-                        }
-                    }
+                    liquidRenderer.material = LiquidRight;
                 }
                 else
                 {
-                    liquidRenderer.material = LiquidNormal;
+                    if (ingredients[substance] > 20)
+                    {
+                        liquidRenderer.material = LiquidTooMuch;
+                    }
+                    else
+                    {
+                        liquidRenderer.material = LiquidNormal;
+                    }
                 }
             }
             if (substance == "Solanine")
             {
-                if (ingredients.ContainsKey(substance))
+                if (Mathf.Round(ingredients[substance]) == 50)
                 {
-                    if (Mathf.Round(ingredients[substance]) == 50)
-                    {
-                        liquidRenderer.material = LiquidRight;
-                    }
-                    else
-                    {
-                        if (ingredients[substance] > 50)
-                        {
-                            liquidRenderer.material = LiquidTooMuch;
-                        }
-                        else
-                        {
-                            liquidRenderer.material = LiquidNormal;
-                        }
-                    }
+                    liquidRenderer.material = LiquidRight;
                 }
                 else
                 {
-                    liquidRenderer.material = LiquidNormal;
+                    if (ingredients[substance] > 50)
+                    {
+                        liquidRenderer.material = LiquidTooMuch;
+                    }
+                    else
+                    {
+                        liquidRenderer.material = LiquidNormal;
+                    }
                 }
             }
         }
@@ -191,19 +238,25 @@ public class Syringe : DataSaver
 
     private void CheckCompletion()
     {
-        if(Manager!=null)
+        if (Manager != null)
             Lable = Manager.CheckCompletion(ingredients);
     }
 
     public void Empty(float time)
     {
-       
+        totalSubstance = syringeML;
+
         if (totalSubstance != 0)
         {
+            if (InnerPart.GetComponent<ConfigurableJoint>())
+            {
+                Destroy(InnerPart.GetComponent<ConfigurableJoint>());
+            }
             StartCoroutine(EmptyingAnimation(time, 0.3f));
             Pomp.transform.parent = InnerPart.transform;
             GetComponent<Grabbable>().enabled = false;
         }
+
     }
 
     IEnumerator EmptyingAnimation(float time, float val)
@@ -244,28 +297,97 @@ public class Syringe : DataSaver
     // Update is called once per frame
     void Update()
     {
+        if (checkPositionInner)
+        {
+            //float scaleDelta=Mathf.
+            indicate.localScale = new Vector3(1, InnerPart.transform.localPosition.y * (-1), 1);
+
+            syringeML = InnerPart.transform.localPosition.y * -50;
+            AmountText.text = syringeML.ToString("0.0");
+
+            if (dopamine)
+            {
+                currentAmountSubstance = amountDopamine + (syringeML - currentAmountSyringe);
+            }
+            if (saline)
+            {
+                currentAmountSubstance = amountSaline + (syringeML - currentAmountSyringe);
+            }
+
+
+
+            if (currentAmountSubstance > amount - inaccuracy && currentAmountSubstance < amount + inaccuracy)
+            {
+                indicate.GetComponentInChildren<Renderer>().material = LiquidNormalCorrect;
+            }
+            if (currentAmountSubstance < amount - inaccuracy /*&& indicateMaterial == LiquidNormalCorrect*/)
+            {
+                indicate.GetComponentInChildren<Renderer>().material = LiquidNormal;
+            }
+            if (currentAmountSubstance > amount + inaccuracy /*&& indicateMaterial == LiquidNormalCorrect*/)
+            {
+                indicate.GetComponentInChildren<Renderer>().material = LiquidNormalUncorrect;
+            }
+
+        }
+
+        //if (checkPositionInner)
+        //{
+        //    //float scaleDelta=Mathf.
+        //    indicate.localScale = new Vector3(1, InnerPart.transform.localPosition.y * (-1), 1);
+
+        //    syringeML = InnerPart.transform.localPosition.y * -50;
+        //    AmountText.text = syringeML.ToString("0.0");
+
+        //    if (syringeML > amount - inaccuracy && syringeML < amount + inaccuracy)
+        //    {
+        //        indicate.GetComponentInChildren<Renderer>().material = LiquidNormalCorrect;
+        //    }
+        //    if (syringeML < amount - inaccuracy /*&& indicateMaterial == LiquidNormalCorrect*/)
+        //    {
+        //        indicate.GetComponentInChildren<Renderer>().material = LiquidNormal;
+        //    }
+        //    if (syringeML > amount + inaccuracy /*&& indicateMaterial == LiquidNormalCorrect*/)
+        //    {
+        //        indicate.GetComponentInChildren<Renderer>().material = LiquidNormalUncorrect;
+        //    }
+        //    //if (syringeML < amount + inaccuracy && indicateMaterial == LiquidNormalUncorrect)
+        //    //{
+        //    //    indicateMaterial = LiquidNormalCorrect;
+        //    //}
+        //}
+
+        if (!setupInInspector)
+        {
+            DataInterface.TryGetItem("SyringeCapacity", ref SyringeCapacity);
+            DataInterface.TryGetItem("SyringeSensitivity", ref SyringeSensitivity);
+        }
         if (pulling)
         {
             float pullAmount = Mathf.Min(med.Amount, Time.deltaTime * SyringeSensitivity, SyringeCapacity - totalSubstance);
             totalSubstance += pullAmount;
-            InnerPart.transform.localPosition = 
-                new Vector3(innerPartPositionInit.x, 
-                Mathf.Lerp(innerPartPositionInit.y, innerPartPositionInit.y - MaxInnerPartDisplacement, totalSubstance / SyringeCapacity), 
+            InnerPart.transform.localPosition =
+                new Vector3(innerPartPositionInit.x,
+                Mathf.Lerp(innerPartPositionInit.y, innerPartPositionInit.y - MaxInnerPartDisplacement, totalSubstance / SyringeCapacity),
                 innerPartPositionInit.z);
             Liquid.transform.localPosition =
                 new Vector3(LiquidPositionInit.x,
-                Mathf.Lerp(LiquidPositionInit.y, LiquidPositionInit.y - (MaxInnerPartDisplacement/2), totalSubstance / SyringeCapacity),
+                Mathf.Lerp(LiquidPositionInit.y, LiquidPositionInit.y - (MaxInnerPartDisplacement / 2), totalSubstance / SyringeCapacity),
                 LiquidPositionInit.z);
             Liquid.transform.localScale =
                 new Vector3(Liquid.transform.localScale.x,
                 Mathf.Lerp(0, MaxLiquidScale, totalSubstance / SyringeCapacity),
                 Liquid.transform.localScale.z);
             ingredients[med.Substance] += pullAmount;
-            if(!med.Infinite)
+            if (!med.Infinite)
                 med.Amount -= pullAmount;
             //AmountText.text = ingredients[med.Substance].ToString("0.0");
             AmountText.text = totalSubstance.ToString("0.0");
             CheckCompletion();
+            foreach (string ingred in ingredients.Keys.ToList())
+            {
+                DataInterface.SendDataItem(ingred, (int)ingredients[ingred]);
+            }
             UpdateGuidedLiquidColor(med.Substance);
         }
         else
@@ -273,7 +395,7 @@ public class Syringe : DataSaver
             if (pushing && (med != null))
             {
                 float delta = Mathf.Min(Time.deltaTime * SyringeSensitivity, totalSubstance);
-                if(delta >= totalSubstance)
+                if (delta >= totalSubstance)
                 {
                     foreach (string ingred in ingredients.Keys.ToList())
                     {
@@ -305,49 +427,15 @@ public class Syringe : DataSaver
                         Mathf.Lerp(0, MaxLiquidScale, totalSubstance / SyringeCapacity),
                         Liquid.transform.localScale.z);
                 }
+                foreach (string ingred in ingredients.Keys.ToList())
+                {
+                    DataInterface.SendDataItem(ingred, (int)ingredients[ingred]);
+                }
                 UpdateGuidedLiquidColor(med.Substance);
             }
         }
         Vector3 targetPosition = new Vector3(Head.transform.position.x, Head.transform.position.y, Head.transform.position.z);
         MeasurementCanvas.transform.LookAt(targetPosition);
         MeasurementCanvas.transform.position = transform.position + Offset;
-    }
-
-    private void Start()
-    {
-        SyringeCap.Sleep();
-        if (Guided)
-        {
-            Expirable _exp;
-            if (TryGetComponent<Expirable>(out _exp))
-            {
-                ExpiredHint.SetActive(_exp.Expired);
-            }
-        }
-    }
-
-    public override void Save()
-    {
-        if(ingredients != null)
-            savedIngredients = new Dictionary<string, float>(ingredients);
-        SavedLiquidMat = liquidRenderer.material;
-    }
-
-    public override void Load()
-    {
-        if (savedIngredients != null)
-        {
-            ingredients = new Dictionary<string, float>(savedIngredients);
-            totalSubstance = ingredients.Values.Sum();
-            Debug.Log("total substitance: " + totalSubstance);
-            AmountText.text = totalSubstance.ToString("0.0");
-        }
-        else
-        {
-            ingredients = new Dictionary<string, float>();
-            totalSubstance = 0;
-        }
-        
-        liquidRenderer.material = SavedLiquidMat;
     }
 }

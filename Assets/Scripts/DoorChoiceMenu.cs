d7183f68d5bf092d1a3b4fddccc60203587ff8c1
Oleg-A-LLIto <Fedol1408@gmail.com>
2024-02-15 22:36:00 +0400

    Feature: added a multiple choice scene menu to a waiting room door, fixed some admission room bugs

--- Assets/Scripts/DoorChoiceMenu.cs
+++ Assets/Scripts/DoorChoiceMenu.cs
@@ -26,7 +26,7 @@ public class DoorChoiceMenu : MonoBehaviour
         }
     }
 
-    private void SubmitSelection()
+    public void SubmitSelection()
     {
         Toggle selectedToggle = GetSelectedToggle();
 

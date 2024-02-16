1badd1c9d5b432a11cbbdcb33e1c5d39c38953e1
Roman <murman89@yandex.ru>
2024-02-16 10:01:06 +0300

    add animations

---
+++ Assets/Scripts/SensorControllerB12.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SensorControllerB12 : MonoBehaviour
+{
+    [SerializeField] GameObject sensor1;
+    [SerializeField] GameObject sensor2;
+    [SerializeField] GameObject sensor3;
+    [SerializeField] GameObject thermometer1;
+    [SerializeField] GameObject thermometer2;
+    //[SerializeField] GameObject thermometer3;
+    [SerializeField] GameObject bloodPressure1;
+    [SerializeField] GameObject bloodPressure2;
+    [SerializeField] GameObject bloodPressure3;
+    public UnityEvent putSensor;
+    public UnityEvent putThermometer;
+    public UnityEvent putBloodPressure;
+    public bool sensorAnimation;
+    public bool thermometerAnimation;
+    public bool bloodPressureAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void TakeSensor()
+    {
+        if (sensorAnimation)
+        {
+            sensor1.SetActive(false);
+            sensor2.SetActive(true);
+        }
+        if (thermometerAnimation)
+        {
+            thermometer1.SetActive(false);
+            thermometer2.SetActive(true);
+        }
+        if (bloodPressureAnimation)
+        {
+            bloodPressure1.SetActive(false);
+            bloodPressure2.SetActive(true);
+        }
+
+    }
+
+    public void PutSensor()
+    {
+        if (sensorAnimation)
+        {
+            sensor2.SetActive(false);
+            sensor3.SetActive(true);
+            putSensor?.Invoke();
+            sensorAnimation = false;
+        }
+        if (thermometerAnimation)
+        {
+            thermometer2.SetActive(false);
+            //thermometer3.SetActive(true);
+            putThermometer?.Invoke();
+            thermometerAnimation = false;
+        }
+        if (bloodPressureAnimation)
+        {
+            bloodPressure2.SetActive(false);
+            bloodPressure3.SetActive(true);
+            putBloodPressure?.Invoke();
+            bloodPressureAnimation = false;
+        }
+
+    }
+}

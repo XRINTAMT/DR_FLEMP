98e83c5b69ee6424c0213bf9705f492847994a74
Oleg-A-LLIto <Fedol1408@gmail.com>
2024-02-22 01:21:38 +0400

    Feature: new left hand menu

--- Assets/Scripts/LongButtonPress.cs
+++ Assets/Scripts/LongButtonPress.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 public class LongButtonPress : MonoBehaviour
 {
     public UnityEvent OnHeldForLongEnough;
+    public UnityEvent OnBriefClick;
     public float Timeout = 2.0f;
 
     private bool buttonPressed;
@@ -29,6 +30,17 @@ public class LongButtonPress : MonoBehaviour
 
     public void Released()
     {
+        Debug.Log("released");
+        if (buttonPressed)
+        {
+            float heldDuration = Time.time - pressTime;
+            Debug.Log(heldDuration);
+            if (heldDuration < Timeout)
+            {
+                Debug.Log("TurningTHeMenuOn");
+                OnBriefClick.Invoke();
+            }
+        }
         buttonPressed = false;
     }
 }
